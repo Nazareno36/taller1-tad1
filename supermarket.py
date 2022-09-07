6fdc2077ca3d2031ae3a83ecd362119f7e27752b
Carlos Paez <carloschiti.12@gmail.com>
2022-09-07 15:17:30 -0500

    Punto B. Añadiendo producto y existencias usando tuplas

--- supermarket.py
+++ supermarket.py
@@ -8,3 +8,10 @@ class Supermarket:
         self.dairy_products = []
         self.groomig_products = []
         self.grain_products = []
+
+    def add(self, item, stock, category):
+        ##Añadiendo tuplas con el producto y las existencias a la lista indicada.
+        if category == 'dairy': self.dairy_products.append((item,stock))
+        if category == 'grooming': self.grooming_products.append((item,stock))
+        if category == 'grain': self.grain_products.append((item,stock))
+
