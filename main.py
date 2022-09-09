8b1dcc9c439914a9ec8935c6698e8a28a7c323a6
Carlos Paez <carloschiti.12@gmail.com>
2022-09-08 20:24:45 -0500

    Punto E. Mostrando inventario

--- main.py
+++ main.py
@@ -15,3 +15,9 @@ supermarket.add('Yogourt', 10, 'dairy')
 ##Punto C.
 supermarket.input_add()
 print(supermarket.dairy_products)
+
+##Punto D.
+supermarket.add('Yogourt',20,'dairy')
+
+##Punto E.
+supermarket.show_inventary()
--- supermarket.py
+++ supermarket.py
@@ -1,4 +1,5 @@
 from itertools import product
+from typing import Tuple
 
 
 class Supermarket:
@@ -6,14 +7,14 @@ class Supermarket:
     def __init__(self):
         ##Declaracion de las listas con los productos.
         self.dairy_products = []
-        self.groomig_products = []
+        self.grooming_products = []
         self.grain_products = []
 
     def add(self, item, stock, category):
         ##Añadiendo tuplas con el producto y las existencias a la lista indicada.
-        if category == 'dairy': self.dairy_products.append((item,stock))
-        if category == 'grooming': self.grooming_products.append((item,stock))
-        if category == 'grain': self.grain_products.append((item,stock))
+        if category == 'dairy': self.validate(item,stock,self.dairy_products)
+        if category == 'grooming': self.validate(item,stock,self.groomig_products)
+        if category == 'grain': self.validate(item,stock,self.grain_products)
 
     def input_add(self):
         ##Recibiendo datos por consola
@@ -34,3 +35,22 @@ class Supermarket:
         
         self.add(item,stock,category)
 
+    def validate(self, item, stock, list):
+        ##Valida si el producto se encuentra ya en el inventario
+        # añade el stock o lo adiciona al inventario si no estaba 
+        found = False
+        i = 0
+        while i < len(list) and not found:
+            if list[i][0] == item:
+                new_stock = list[i][1] + stock 
+                list[i] = (item,new_stock)
+                found = True
+            i+=1        
+        if not found: list.append((item,stock))    
+    
+    ##Mostrando el inventario.
+    def show_inventary(self):
+        print('\tInventario\t')
+        print(f"Productos lacteos: {self.dairy_products}")
+        print(f"Productos de aseo: {self.grooming_products}")
+        print(f"Productos de grano: {self.grain_products}")
